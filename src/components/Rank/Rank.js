c28bc04e1bce1905f416bb59ed31d191cc3230d1
Willy-Friggin-V <williamventura711@gmail.com>
2023-06-16 13:57:34 -0400

    layout

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Rank = () => {
+  return(
+      <div>
+        <div className='white f3'>
+            {'Will, your current Rank is...'}
+        </div>
+        <div className='white f3'>
+            {'#5'}
+        </div>
+      </div>
+  );
+}
+export default Rank;
