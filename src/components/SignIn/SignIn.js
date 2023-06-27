3907bc5dca9da48fff7671388394e661f8b23388
Willy-Friggin-V <williamventura711@gmail.com>
2023-06-27 16:32:10 -0400

    fixed url reference

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
     }
 
     OnSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://server-4sm8.onrender.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
